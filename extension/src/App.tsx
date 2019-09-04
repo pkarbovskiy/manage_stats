e1c86e769af529fe7fd7a6804b8b738995bd200a
Karbovskiy, Pavel <pavel.karbovskiy@kronos.com>
2019-09-04 16:36:54 -0400

    some fixes

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,38 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-function tableCheckboxes(ids: string[]) {
-    Array.prototype.slice.apply(document.querySelectorAll('.video tr')).forEach(val => {
-        const id = val.dataset.href.split('/')[2];
-        console.log(id);
-        const input = document.createElement('input');
-        input.setAttribute('type', 'checkbox');
-        input.setAttribute('value', id)
-        input.addEventListener('click', (e) => {
-            e.stopPropagation();
-            if (ids.includes(id)) {
-                ids = ids.splice(ids.indexOf(id), 1);
-                return;
-            }
-            console.log(ids);
-            ids.push(id);
-        });
-        val.childNodes[3].prepend(input);
-    });
-    return ids;
-}
-const App: React.FC = () => {
-    let ids: string[] = [];
-    useEffect(() => {
 
-    });
+const App: React.FC = () => {
+    const [ids, setIds] = useState([] as string[]);
+    let runOnce = false;
+    useEffect(() => {
+            Array.prototype.slice.apply(document.querySelectorAll('.video tr')).forEach(val => {
+                const id: string = val.dataset.href.split('/')[2];
+                const input = document.createElement('input');
+                input.setAttribute('type', 'checkbox');
+                input.setAttribute('value', id)
+                input.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    if (ids.includes(id)) {
+                        setIds(ids => ids.splice(ids.indexOf(id), 1));
+                        return;
+                    }
+                    setIds(ids => [...ids, id]);
+                });
+                val.childNodes[3].prepend(input);
+            });
+    }, []);
     return (
         <>
             <div className="main">
-                <button onClick={() => { tableCheckboxes(ids); }}>Edit</button>
+                <button onClick={() => { void 0 }}>Apply</button>
             </div>
             <ul>
                 {ids.map(id => {
-                    console.log(id);
                     return (<li key={id}>{id}</li>);
                 })}
             </ul>
